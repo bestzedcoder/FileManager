2ee61195207b3d4b2b2830ff05acc671f2388732
phongbeovaio <lenamphongsoict@gmail.com>
2024-12-23 13:50:42 +0700

    First Time Build This Project !

---
+++ 15102024/app/src/main/java/com/example/a15102024/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.a15102024
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val itemm= mutableListOf<String>()
+        for(i in 1 <= ... <= 50 )
+            items.add("Item $i")
+
+        val adapter
+        }
+    }
